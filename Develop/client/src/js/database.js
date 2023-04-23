36cc3ebd4bfa793ba20a83e2f5f06949d6d4e8b6
xAsdfaf <georgejordanjr8@gmail.com>
2023-04-23 12:44:56 -0500

    added a ton of code, using the class repo as reference, and modified code from class repo.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,9 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+console.error('putDb not implemented');
 
 const jateDb = await openDB('jate', 1);
 
@@ -26,7 +28,21 @@ const req = store.add({content: content})
 const result = await req;
 
 console.log('Data saved to the database', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const req = store.getAll();
+
+  const result = await req;
+  console.log('result.value', result);
+};
 
 initdb();
